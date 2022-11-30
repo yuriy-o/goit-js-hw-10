450095c51ff7a1e1b65bbc0267ac9ae0a5f46bbf
Yuriy <sharik.kiev@gmail.com>
2022-11-30 03:03:34 +0200

    theory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchParams = new URLSearchParams({
+  _limit: 5,
+  _sort: 'name',
+});
+
+console.log(searchParams.toString());
+
+fetch(`https://jsonplaceholder.typicode.com/users?${searchParams}`)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(console.log);
