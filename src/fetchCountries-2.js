3734270a1ed9c882e98c22d6795f625cb7c6fde8
Yuriy <sharik.kiev@gmail.com>
2022-12-06 10:48:58 +0200

    Update fetchCountries-2.js

--- src/fetchCountries-2.js
+++ src/fetchCountries-2.js
@@ -1,8 +1,16 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // const axios = require('axios');
 import axios from 'axios';
 
 export function fetchCountries(name) {
-  return axios.get(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  );
+  return axios
+    .get(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    )
+    .then(response => {
+      console.log('then#1 → response', response);
+      console.log('then#1 → response.data', response.data);
+
+      return response.data;
+    });
 }
